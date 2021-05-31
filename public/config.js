1eb84efd9a329b805072bbafc678b7b4f180ff76
Abner Rodrigues <40338524+rodriguesabner@users.noreply.github.com>
2021-05-31 14:09:03 -0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.IP_SERVER = "https://wppserver.herokuapp.com/api/";
-window.IP_SOCKET_IO = "https://wppserver.herokuapp.com/";
+window.IP_SERVER = "http://192.168.15.46:21465/api/";
+window.IP_SOCKET_IO = "http://192.168.15.46:21465/";
